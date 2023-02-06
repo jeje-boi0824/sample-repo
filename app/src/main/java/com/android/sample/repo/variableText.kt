fe7c79eb9fc4b5692f7cc438f69f02696e168c36
jeje-boi0824 <kodegomd3p.jerome.elenzano@gmail.com>
2023-02-06 20:37:01 +0800

    second commit

---
+++ app/src/main/java/com/android/sample/repo/variableText.kt
@@ -0,0 +1,83 @@
+package com.android.sample.repo
+
+import kotlin.text.Typography.times
+
+fun main(){
+//    val dateSample = LocalDate.now()
+//    val randomSample = Random.nextInt
+
+//    var boleanSample = true
+//    var boleanSample = false
+
+//    println(int1==int2)
+//    println(int1>=int2)
+//
+//    var int1 = 4
+//    println("Enter number: ")
+//    var int2 = readln().toInt()
+//
+//    var addedTotal = int1 + int2
+
+//    if (addedTotal == 6){
+//        println("Condition 1 = $addedTotal")
+//    } else if ("Condition 2 = $addedTotal == 7){
+//        println(addedTotal)
+
+//    var intA = 10
+//    var intB = 10
+//    when(intA + intB == 20){
+//        true -> {
+//            println("available")
+//        } else -> {
+//            println("else")
+//        }
+    println("Control Struture")
+//    repeat(times:5) { count ->
+//        val countB = count + 1
+//        println("repeat $count")
+
+//   var counterWhile = 1
+//    while (counterWhile < 10) {
+//        counterWhile++
+//        println("while $counterWhile")
+//    }
+
+
+//    while , DO and break, continue
+    var counterDo = 1
+    do {
+        println("do $counterDo")
+        counterDo++
+        if (counterDo ==5) {
+            break
+        }
+    } while (counterDo <=10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
